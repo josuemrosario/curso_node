f7f33618394e008a557540495f1dc51237537b5e
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2023-01-09 10:51:33 -0300

    Aula 8 - Cria um Servidor Basico
    Cria um servidor básico com 1 resposta

--- Noticias.js
+++ Noticias.js
@@ -1,1 +1,8 @@
-console.log('Criando um site de noticias com NodeJs');
+var http = require('http');
+
+// Aula 8 - Servidor basico de resposta
+var server = http.createServer( function(req,res){
+    res.end("<html><body>Portal de Notícias</body></html>"); // Resposta
+});
+
+server.listen(3000);
