2abacc144edfc08358dcdaba72ecc6651e0f3c6f
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2023-01-09 11:04:56 -0300

    Seção 2 - Aula 9 - Acrescentando Rotas Manualmente baseado na URL informada

--- Noticias.js
+++ Noticias.js
@@ -2,7 +2,21 @@ var http = require('http');
 
 // Aula 8 - Servidor basico de resposta
 var server = http.createServer( function(req,res){
-    res.end("<html><body>Portal de Notícias</body></html>"); // Resposta
+
+    var categoria = req.url;
+    console.log(categoria)
+
+    // Aula 9 - Configurando Rotas manualmente
+    if(categoria == '/tecnologia'){
+        res.end("<html><body>Noticias de Tecnologia</body></html>"); 
+    }else if (categoria == '/moda'){
+        res.end("<html><body>Noticias de Moda</body></html>"); 
+    }else if (categoria == '/beleza'){
+        res.end("<html><body>Noticias de Beleza</body></html>"); 
+    }else {
+        res.end("<html><body>Portal de Notícias</body></html>"); 
+    }
+    
 });
 
 server.listen(3000);
