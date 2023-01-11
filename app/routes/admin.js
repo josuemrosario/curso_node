b060072a01721820d521a6e7e52cf645bf0cb370
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2023-01-11 11:08:47 -0300

    SECAO 8 - aula 40 - add noticias no banco
    - criado método salvarNoticia dentro do model noticiasModel
    - Modificado a rota noticias/salvar para enviar a noticia para noticiasModel@salvarNoticia() e redirecionar para /noticias que faz o load de todas as notícias

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,13 +1,25 @@
-module.exports = function(app){
+module.exports = function(application){
 
-    app.get('/formulario_inclusao_noticia',function(req,res){
+    application.get('/formulario_inclusao_noticia',function(req,res){
         res.render("admin/form_add_noticia") //renderiza uma view
     });
 
     //aula 39
-    app.post('/noticias/salvar',function(req,res){
-        var noticias = req.body;  //funciona apenas com body parser instalado
-        res.send(noticias);
+    application.post('/noticias/salvar',function(req,res){
+        
+        var noticia = req.body;  //funciona apenas com body parser instalado
+        //res.send(noticias);
         //res.render("admin/form_add_noticia") //renderiza uma view
+
+        //aula 40
+        var connection = application.config.dbConnection();
+        var noticiasModel = application.app.models.noticiasModel;
+        
+        noticiasModel.salvarNoticia(noticia,connection,function(erro,result){
+            //res.render("noticias/noticias", {noticias : result});
+            //obs: render não funciona porque permite F5 que reenvia os dados e duplica no banco
+            res.redirect('/noticias');
+        })
+
     });
 };    
