1eddeda8a4a72eff9b607be36e1eb46a8918c3b0
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2023-01-12 14:39:51 -0300

    Secao 12 - Aula 56
    Ajusta o controller noticias.js para garantir que o id da noticia clicada chegue em noticiasDAO

--- app/Controllers/noticias.js
+++ app/Controllers/noticias.js
@@ -11,7 +11,10 @@ module.exports.noticia = function(application, req, res){
     var connection = application.config.dbConnection();
     var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
-    noticiasModel.getNoticia(function(erro,result){
+    //Aula 56 - Ajusta para que o parametro passado por url chegue no model NoticiasDAO
+    var id_noticia = req.query
+
+    noticiasModel.getNoticia(id_noticia,function(erro,result){
         res.render("noticias/noticia", {noticia : result});
     });
 
