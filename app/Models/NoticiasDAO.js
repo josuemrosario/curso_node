9587d41d34526ce584e0bf199e22bb160babc115
josue Micaroni do Rosario <josuemrosario@yahoo.com.br>
2023-01-12 14:26:23 -0300

    Secao 12 - Aula 55
    - Ajustado view noticia.ejs para mostra a noticia

--- app/Models/NoticiasDAO.js
+++ app/Models/NoticiasDAO.js
@@ -4,11 +4,11 @@ function NoticiasDAO(connection){
 }
 
 NoticiasDAO.prototype.getNoticias = function(callback){
-    this._connection.query('select * from noticias',callback);
+    this._connection.query('select * from noticias order by data_criacao desc',callback);
 } 
 
 NoticiasDAO.prototype.getNoticia = function(callback){
-    this._connection.query('select * from noticias where id_noticia = 2',callback);
+    this._connection.query('select * from noticias where id_noticia = 10',callback);
 } 
 
 NoticiasDAO.prototype.salvarNoticia = function(noticia,callback){
